ec7565663a034fe22d30b0ab24fb73a60ce3157b
trbmcginnis <taylormcginnis@TaylorMnnissMBP.fios-router.home>
2017-09-08 10:06:53 -0400

    Here is assignment 1

---
+++ Assignment_1_Javascript.js
@@ -0,0 +1,9 @@
+// arrays
+var thingsArray = ["hiking", "reading", "looking at rocks", "music shows"];
+
+
+// for loop through an array
+for (var i = 0; i < thingsArray.length; i++) {
+	console.log("Favorite Thing", i + 1, thingsArray[i]);
+}
+
